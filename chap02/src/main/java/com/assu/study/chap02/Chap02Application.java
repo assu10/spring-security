e16dfdd847a88a504b4aa42b4199f7cf48b3cdd2
assu10 <juhyun10.lee@gmail.com>
2024-02-10 16:37:39 +0900

    first

---
+++ chap02/src/main/java/com/assu/study/chap02/Chap02Application.java
@@ -0,0 +1,13 @@
+package com.assu.study.chap02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap02Application.class, args);
+	}
+
+}
---
+++ chap02/src/test/java/com/assu/study/chap02/Chap02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assu.study.chap02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
