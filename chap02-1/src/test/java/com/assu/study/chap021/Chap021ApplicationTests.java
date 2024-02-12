abee12ae0cb7ca51f3007146902a0b74c96d97ed
assu10 <juhyun10.lee@gmail.com>
2024-02-12 11:58:54 +0900

    3.1. `UserDetailsService` 재정의

---
+++ chap02-1/src/test/java/com/assu/study/chap021/Chap021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assu.study.chap021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap021ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
